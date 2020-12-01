54c92bc1305eb1f98308d60ef4ad449603dde84b
danjarosz <jarosz.daniel@protonmail.com>
2020-12-01 20:19:19 +0100

    feat: prepare basic theme and ThemeProvider

---
+++ src/theme/theme.js
@@ -0,0 +1,27 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00796b',
+      light: '#48a999',
+      dark: '#004c40',
+      contrastText: '#fff'
+    },
+    secondary: {
+      main: '#795548',
+      light: '#a98274',
+      dark: '#4b2c20',
+      contrastText: '#fff'
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#ccc',
+    },
+  }
+});
+
+export default theme;
