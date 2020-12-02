3b45b29f3405a7e270520a4208056b8cb8291b61
danjarosz <jarosz.daniel@protonmail.com>
2020-12-02 18:29:21 +0100

    feat: prepare components for pages

---
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Box, Typography } from "@material-ui/core";
+
+const DashboardPage = () => {
+  return (
+    <Box>
+      <Typography>Dashboard page</Typography>
+    </Box>
+  )
+};
+
+export default DashboardPage;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import PublicRoute from "./components/PublicRoute";
-import PrivateRoute from "./components/PrivateRoute";
+// import PublicRoute from "./components/PublicRoute";
+// import PrivateRoute from "./components/PrivateRoute";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import DashboardPage from "../pages/DashboardPage/DashboardPage";
+import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 
 export const history = createBrowserHistory();
 
-const AppRouter = () => {
+const AppRouter = () => (
   <Router history={history}>
     <div>
       <Switch>
-        <PublicRoute exact path="/" component={LoginPage}/>
-        <PublicRoute exact path="/dashboard" component={DashboardPage}/>
+        <Route exact path="/" component={LoginPage}/>
+        <Route exact path="/dashboard" component={DashboardPage}/>
         <Route component={NotFoundPage}/>
       </Switch>
     </div>
   </Router>
-}
+);
 
 export default AppRouter;
