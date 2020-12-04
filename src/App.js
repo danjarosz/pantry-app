6fe3fa5f8d54dba785fc9fdfa6d9db870b5c2fb4
Daniel Jarosz <74425129+danjarosz@users.noreply.github.com>
2020-12-04 18:50:34 +0100

    Firebase config (#9)
    * Configure firebase (#8)
    
    * chore: add firebase
    
    * chore: update .gitignore
    
    * feat(firebase.js): config firebase connection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "@material-ui/core";
+import database from "./firebase/firebase";
 import AppRouter from "./router/AppRouter";
 
 function App() {
+
+  useEffect(() => {
+    database.ref("/pantries").once("value").then((snapshot) => {
+      console.log(snapshot.val())
+    })
+  }, []);
+
   return (
     <Container maxWidth="md">
       <AppRouter />
