b34b457cf697e7e976c837b9b88caf74935e3082
danjarosz <jarosz.daniel@protonmail.com>
2020-12-04 21:16:42 +0100

    feat(LoginPage): basic layout

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,67 @@
 import React from 'react';
-import { Box, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import { 
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography
+} from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  Container: {
+    minHeight: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center"
+  }
+}));
 
 const LoginPage = () => {
+  const classes = useStyles();
+
   return (
-    <Box>
-      <Typography>Login page</Typography>
-    </Box>
+    <Container
+      maxWidth="xs"
+      className={classes.Container}
+    >
+      <Box>
+        <Box>
+          <Typography
+            variant="h4"
+            align="center"
+          >
+            Logowanie
+          </Typography>
+        </Box>
+        <Box my={3}>
+          <TextField
+            label={"e-mail"}
+            fullWidth={true}
+            variant="outlined"
+            required={true}
+          />
+        </Box>
+        <Box my={3}>
+          <TextField
+            label={"hasło"}
+            fullWidth={true}
+            variant="outlined"
+            required={true}
+            type="password"
+          />
+        </Box>
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth={true}
+          type="submit"
+        >
+          Zaloguj się
+        </Button>
+      </Box>
+    </Container>
   )
 };
 
