3b7ab3e31cfdf06580b83021372e13db8ca36be7
danjarosz <jarosz.daniel@protonmail.com>
2020-12-05 21:13:15 +0100

    feat: implement AppBar

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
+import TopBar from "../components/TopBar/TopBar";
 import PublicRoute from "./components/PublicRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import LoginPage from "../pages/LoginPage/LoginPage";
@@ -12,14 +13,13 @@ export const history = createBrowserHistory();
 
 const AppRouter = () => (
   <Router history={history}>
-    <div>
+      <TopBar />
       <Switch>
         <PublicRoute exact path="/" component={LoginPage}/>
         <PublicRoute exact paths="/signup" component={SignUpPage}/>
         <PrivateRoute exact path="/dashboard" component={DashboardPage}/>
         <Route component={NotFoundPage}/>
       </Switch>
-    </div>
   </Router>
 );
 
