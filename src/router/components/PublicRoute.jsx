07db469da9e5bb44c9e554f4fd44ee311ed35342
Daniel Jarosz <74425129+danjarosz@users.noreply.github.com>
2020-12-07 17:26:10 +0100

    Dev (#21)
    * feat: improvements (#20)

--- src/router/components/PublicRoute.jsx
+++ src/router/components/PublicRoute.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
+import TopBar from "../../components/TopBar/TopBar";
 
 const PublicRoute = ({ component: Component, ...rest }) => {
     const isAuthenticated = useSelector(({ auth }) => auth.uid);
     return <Route {...rest} component={(props) => (
         !isAuthenticated ? (
-          <Component {...props} />
+          <>
+            <TopBar variant="public"/>
+            <Component {...props} />
+          </>
         ) : (
           <Redirect to="/dashboard" />
         )
