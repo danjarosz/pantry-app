ef19f4149672066cfd4889f718385308f9c438fb
danjarosz <jarosz.daniel@protonmail.com>
2020-12-30 20:13:18 +0100

    feat: configure redux to login with Google

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,2 +1,6 @@
 // Settings
 export const SET_LANGUAGE = "SET_LANGUAGE";
+
+//LOGIN
+export const LOGIN = "LOGIN";
+export const LOGOUT = "LOGOUT";
---
+++ src/redux/actions/authActions.js
@@ -0,0 +1,27 @@
+import { firebase, googleAuthProvider } from "../../firebase/firebase";
+import { LOGIN, LOGOUT } from "../actionTypes";
+
+export const login = (uid) => {
+  return {
+    type: LOGIN,
+    uid,
+  };
+};
+
+export const startLogin = () => {
+  return (dispatch) => {
+    return firebase.auth().signInWithPopup(googleAuthProvider);
+  };
+};
+
+export const logout = () => {
+  return {
+    type: LOGOUT,
+  };
+};
+
+export const startLogout = () => {
+  return (dispatch) => {
+    return firebase.auth().signOut();
+  };
+};
